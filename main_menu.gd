4044e219242b60d9275bf9284a7d1a8af08b832c
Ksong228 <ks3kf@mail.umsl.edu>
2024-11-04 15:55:20 -0600

    Change menu layouts
    Changed the main menu and lobby layout and added back some button functionalities

--- main_menu.gd
+++ main_menu.gd
@@ -1,19 +1,13 @@
 extends Control
 
 func _on_single_player_pressed() -> void:
-	get_tree().change_scene_to_file("insert scenery")
-
-
-func _on_create_game_pressed() -> void:
-	pass # Replace with function body.
-
-
+	get_tree().change_scene_to_file("res://lobby menu.tscn")
+	
+func _on_host_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://lobby menu.tscn")
+	
 func _on_join_game_pressed() -> void:
 	pass # Replace with function body.
 
-
-func _on_resume_pressed() -> void:
-	pass # Replace with function body.
-
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://lobby.tscn")
+func _on_exit_pressed() -> void:
+	get_tree().exit
