b3fea80c8e4575c42b88a43f6d628558569fe700
Mason Golslee <mjgqhq@umsystem.edu>
2024-11-10 21:49:51 -0600

    Added Mizuno scene and Solitare button functionalities and attempted to add example solitare
    made a scene for mizuno and added button functionality for the lobby. Also added button functionality for the Solitare button in the lobby scene. I added the addon folder for card3d framework but I couldn't get it working, project crashes when attempting to open.

---
+++ scripts/lobby menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main menu.tscn")
+
+func _on_mizuno_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Mizuno.tscn")
+	
+func _on_solitare_pressed() -> void:
+	get_tree().change_scene_to_file("res://addons/card_3d/example_solitaire/scenes/solitaire.tscn")
