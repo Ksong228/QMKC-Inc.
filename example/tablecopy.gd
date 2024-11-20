6702df66f926eb6399c04ca1214889ef8d7c875d
Quinn Reller <61479103+quinnreller55@users.noreply.github.com>
2024-11-19 18:10:29 -0600

    File Rename and some Mizuno Code

--- example/tablecopy.gd
+++ example/tablecopy.gd
@@ -15,25 +15,22 @@ var rank_index = 0
 
 @onready var hand: CardCollection3D = $DragController/Hand
 @onready var pile: CardCollection3D = $DragController/TableCards
-
+@onready var player1: CardCollection3D = $DragController/Player1
+@onready var player2: CardCollection3D = $DragController/Player2
+@onready var player3: CardCollection3D = $DragController/Player3
+@onready var player4: CardCollection3D = $DragController/Player4
 
 func _input(event):
 	if event.is_action_pressed("ui_down"):
 		add_card()
+	elif pile.card_layout_strategy is LineCardLayout:
+		pile.card_layout_strategy = PileCardLayout.new()
+	elif hand.card_layout_strategy is LineCardLayout:
+		pile.card_layout_strategy = PileCardLayout.new()
 	elif event.is_action_pressed("ui_up"):
 		remove_card()
 	elif event.is_action_pressed("ui_left"):
 		clear_cards()
-	elif event.is_action_pressed("ui_right"):
-		if pile.card_layout_strategy is PileCardLayout and hand.card_layout_strategy is LineCardLayout:
-			var layout := LineCardLayout.new()
-			pile.card_layout_strategy = layout
-		elif hand.card_layout_strategy is LineCardLayout:
-			hand.card_layout_strategy = FanCardLayout.new()
-		elif pile.card_layout_strategy is LineCardLayout:
-			pile.card_layout_strategy = PileCardLayout.new()
-		elif hand.card_layout_strategy is FanCardLayout:
-			hand.card_layout_strategy = LineCardLayout.new()
 
 
 func instantiate_face_card(rank, suit) -> FaceCard3D:
--- scripts/lobby menu.gd
+++ scripts/lobby menu.gd
@@ -4,7 +4,7 @@ func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main menu.tscn")
 
 func _on_mizuno_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Mizuno.tscn")
+	get_tree().change_scene_to_file("res://example/mizunotable.tscn")
 	
 func _on_solitare_pressed() -> void:
 	get_tree().change_scene_to_file("res://example_solitaire/scenes/solitaire.tscn")
